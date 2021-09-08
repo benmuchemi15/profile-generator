d87289d0801fb32c545a486592071ce2c948392d
Benson Muchemi <Ben@Bensons-MacBook-Pro.local>
2021-09-08 02:39:28 -0400

    added questions

--- index.js
+++ index.js
@@ -4,20 +4,34 @@ const Intern = require('./lib/intern');
 const Manager = require("./lib/manager");
 
 const newManager = () => {
-   return inquirer.prompt({
+   return inquirer.prompt(
+       {
     // pass your questions in here
     type: 'input',
     name:'name',
     question: 'Would you like to add a manager?',
+       },
+    
+    {
+    type: 'input',
+    name:'id',
+    question: 'What is the manager ID?',
+    },
+    {
+    type: 'input',
+    name:'email',
+    question: 'What is the manager email?',
+    }
+};
 
-})
+
+
+
+
+newManager()
 .then((answers) => {
     //use user feedback
 })
-
-};
-
-newManager()
 .catch((error) => {
     if(error.isTtyError) {
 
