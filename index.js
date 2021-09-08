df164711e2d677d3189a5d8d0ed6ffb7e0bcc7cc
Benson Muchemi <Ben@Bensons-MacBook-Pro.local>
2021-09-08 02:30:00 -0400

    created newManager function

--- index.js
+++ index.js
@@ -2,16 +2,26 @@ const inquirer = require("inquirer");
 const Engineer = require('./lib/engineer');
 const Intern = require('./lib/intern');
 const Manager = require("./lib/manager");
-inquirer.prompt({
+
+const newManager = () => {
+   return inquirer.prompt({
     // pass your questions in here
+    type: 'input',
+    name:'name',
+    question: 'Would you like to add a manager?',
+
 })
 .then((answers) => {
     //use user feedback
 })
+
+};
+
+newManager()
 .catch((error) => {
     if(error.isTtyError) {
 
     }else {
         //something else went wrong
     }
-})
+}) 
